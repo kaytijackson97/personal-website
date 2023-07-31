eca4957d6a7dcb326972e7fa9bbc083f936915d5
kaytiwitia <kaytiwiita@github.com>
2023-07-30 21:46:52 -0400

    creating portfolio project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+import { About } from "./components/pages/About";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
